560b32fa865e603088c9855d5e77165e32117cc1
Sundarapandian, Karpoora <karpoora.sundarapandian@astrazeneca.com>
2017-10-23 22:43:50 +0530

    changes on spring joiner

---
+++ src/main/java/com/learning/java/util/StringJoinDemo.java
@@ -0,0 +1,27 @@
+package com.learning.java.util;
+
+import java.util.StringJoiner;
+
+/**
+ * Created by krvh271 on 10/20/17.
+ */
+public class StringJoinDemo {
+
+    public static void main(String args[]){
+
+        StringJoinDemo joinObject=new StringJoinDemo();
+        joinObject.method1();
+
+    }
+
+    void method1(){
+        StringJoiner joiner=new StringJoiner(",");
+        System.out.println(joiner.length());
+        joiner.add("test1");
+        joiner.add("test2");
+        joiner.add("test3");
+        joiner.add("test4");
+
+        System.out.println(joiner.length());
+    }
+}
